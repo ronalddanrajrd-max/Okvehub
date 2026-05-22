cf133eea7b59fcdda5fa7c8719bcd0a2beb16bc3
ronalddanrajrd-max <ronald.danraj.rd@gmail.com>
2026-05-22 01:41:53 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,37 @@
 import os
 import discord
 from discord.ext import commands
+import asyncio
 
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="!", intents=intents)
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
 
 @bot.event
 async def on_ready():
-    print(f"✅ Bot online: {bot.user}")
-    await bot.tree.sync()
+    print(f"✅ Connected as {bot.user}")
 
-async def load():
+    try:
+        synced = await bot.tree.sync()
+        print(f"✅ Synced {len(synced)} commands")
+    except Exception as e:
+        print(e)
+
+async def load_cogs():
+
     await bot.load_extension("cogs.shop")
     await bot.load_extension("cogs.tickets")
     await bot.load_extension("cogs.payments")
+    await bot.load_extension("cogs.moderation")
     await bot.load_extension("cogs.admin")
 
-import asyncio
-
 async def main():
+
     async with bot:
-        await load()
+        await load_cogs()
         await bot.start(os.getenv("TOKEN"))
 
 asyncio.run(main())
