b6b940bf2caf8e8bb7c842191d313e5f99652156
ronalddanrajrd-max <ronald.danraj.rd@gmail.com>
2026-05-22 01:41:08 -0300

    Update admin.py

--- cogs/admin.py
+++ cogs/admin.py
@@ -1,15 +1,36 @@
 import discord
 from discord.ext import commands
+from database import load, save
 
 class Admin(commands.Cog):
+
     def __init__(self, bot):
         self.bot = bot
 
-    @discord.app_commands.command(name="add-brainrot")
-    async def add(self, interaction: discord.Interaction, user: discord.Member, amount: int):
+    @discord.app_commands.command(
+        name="add-brainrot",
+        description="Add brainrot"
+    )
+    async def add(
+        self,
+        interaction: discord.Interaction,
+        user: discord.Member,
+        amount: int
+    ):
+
+        data = load()
+
+        uid = str(user.id)
+
+        data["brainrot"][uid] = (
+            data["brainrot"].get(uid, 0)
+            + amount
+        )
+
+        save(data)
 
         await interaction.response.send_message(
-            f"🧠 +{amount} brainrot for {user}",
+            f"✅ Added {amount} brainrot to {user}",
             ephemeral=True
         )
 
